871d7f10e860070a71352a50c0c0710470c349df
Alok Kumar <119784446+imAlok12@users.noreply.github.com>
2026-02-20 00:01:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".sos").onclick = function() {
-  alert("SOS Activated! Sending location...");
-};
-
-document.querySelector("button").onclick = function() {
-  let text = document.querySelector("textarea").value;
-  alert("Analyzing: " + text);
-};
